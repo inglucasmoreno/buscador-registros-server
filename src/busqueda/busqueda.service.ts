c1f02e01118fe0527fab3639c6ddbcbb6f43c358
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-03-09 13:09:51 -0300

    Para produccion

--- src/busqueda/busqueda.service.ts
+++ src/busqueda/busqueda.service.ts
@@ -10,12 +10,13 @@ export class BusquedaService {
     const workbookSheets = workbook.SheetNames;
     const sheet = workbookSheets[0];
     const dataExcel: any = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-
+    
     // Verificacion de formato excel
     const condicion = dataExcel.length > 0 &&
                       dataExcel[0].dni &&
                       dataExcel[0].apellido_nombre &&
                       dataExcel[0].domicilio                  
+
 
     if(!condicion) throw new NotFoundException('Excel con formato incorrecto');
 
